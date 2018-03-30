ea8a6ffaf0cba934c1ef972fd98f46be0a7a2360
Avinash <Avinash@Avinash-PC>
2018-03-30 19:15:55 +0530

    changing in arraylistexample

--- src/abhishek/ArrayListExample.java
+++ src/abhishek/ArrayListExample.java
@@ -1,12 +1,11 @@
-package abhishek;
 
+package abhishek;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 
-public class ArrayListExample {
-
-
+public class ArrayListExample
+{
 	public static void main(String args[])
 	{
 	ArrayList<String> list=new ArrayList<String>();
@@ -18,17 +17,28 @@ public class ArrayListExample {
     list.add("2");
 
     int firstIndex = list.indexOf("2");
-    System.out.println("first index :" +firstIndex);
+    System.out.println("first index of 2:" +firstIndex);
     
     int lastIndex = list.lastIndexOf("2");
-    System.out.println("first index :" +lastIndex);
+    System.out.println("last index of 2:" +lastIndex);
     
-    
+    System.out.println("iterating the element : ");
     Iterator<String> itr=list.iterator();
     while(itr.hasNext())
     {
     	System.out.println(itr.next());
     }
+    
+    
+    Collections.sort(list, Collections.reverseOrder());
+    System.out.println("sorting or decending the arraylist :");
+    Iterator<String> itr1=list.iterator();
+    while(itr1.hasNext())
+    {
+    	System.out.println(itr1.next());
+    }
+    
+    
     if(list!=null && !list.isEmpty())
     {
     	System.out.println("last element is :" + list.get(list.size()-1));
